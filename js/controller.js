a6210610c5c9cbe5c4fd433b873f87d8af69fe3f
Meir Sabbah <47792657+promermedia@users.noreply.github.com>
2019-02-25 13:12:48 +0200

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,17 +1,23 @@
+function init(){
+    if (getJson()) {
+    createHTML();
+  };
+};
+
 function addCar() {
   getInputsFromDom();
   createCarObj();
   cleanInputs();
   sendJson();
   createHTML();
-}
+};
 
 function toggle(button) {
   showHide(button);
-}
+};
 
 function deleteCar(car) {
   removeData(car);
   sendJson();
   createHTML();
-}
+};
